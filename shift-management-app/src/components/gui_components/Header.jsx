39ebd731a9f6652923f0c9f0537c5f7fc1b8c0d6
Artem Yablunin <yablunin.artem@gmail.com>
2024-09-18 12:08:42 +0300

    - Added today date to Header block

--- shift-management-app/src/components/gui_components/Header.jsx
+++ shift-management-app/src/components/gui_components/Header.jsx
@@ -3,7 +3,12 @@ import "./Header.css";
 export default function Header() {
   return (
     <>
-      <header></header>
+      <header>
+        <div className="header-today-block">
+          <p className="header-today-title">Сьогодні</p>
+          <p className="header-today-info">{new Date().toLocaleDateString()}</p>
+        </div>
+      </header>
     </>
   );
 }
